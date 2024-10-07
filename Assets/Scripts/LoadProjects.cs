48de09a1f163c55d52b498e9affbb45c21256a85
Chriscraft57 <steiner.dichris@gmail.com>
2024-10-07 10:51:33 +0200

    Adjusted UI + added search bar and chapter select UI

--- Assets/Scripts/LoadProjects.cs
+++ Assets/Scripts/LoadProjects.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class LoadProjects : MonoBehaviour
 {
@@ -32,15 +33,45 @@ public class LoadProjects : MonoBehaviour
         foreach (var project in projects)
         {
             GameObject newButton = Instantiate(buttonPrefab, content);
+
+            // Set button text
             TextMeshProUGUI buttonText = newButton.GetComponentInChildren<TextMeshProUGUI>();
             if (buttonText != null)
             {
+                // Set font size to match black buttons
+                buttonText.fontSize = 20;
+                buttonText.enableAutoSizing = false; // Ensure that text does not auto-scale
+                buttonText.alignment = TextAlignmentOptions.Left; // Adjust alignment to match the look
+
                 buttonText.text = project;
             }
             else
             {
                 Debug.LogError("No TextMeshProUGUI component found on button prefab!");
             }
+
+            // Add the BlockDrag component to prevent dragging
+            BlockDrag blockDrag = newButton.AddComponent<BlockDrag>();
         }
+    }
+}
+
+// This script blocks dragging of the buttons
+public class BlockDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Prevent dragging
+        eventData.pointerDrag = null;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Do nothing
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Do nothing
     }
 }
