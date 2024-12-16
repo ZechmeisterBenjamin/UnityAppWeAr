5f7de2802c0f618ebb3a40026867783be569dcd1
Benjamin Zechmeister <benjamin.zechmeister@htlwy.at>
2024-12-16 11:36:52 +0100

    Load categories and change text when increasing or decreasing the category

--- Assets/Scripts/LoadText.cs
+++ Assets/Scripts/LoadText.cs
@@ -15,13 +15,21 @@ public class LoadText : MonoBehaviour
     public TMP_Dropdown dropdown;
     public TMP_Text title;
     public TMP_Text text;
+    public TMP_Text category;
     public Button button;
 
     private void Start()
     {
         dropdown.onValueChanged.AddListener(delegate { LoadTextValue(dropdown.value); });
+
+        category.GetComponent<TMP_InputField>().onEndEdit.AddListener(delegate { CategoryTextChanged(); });
     }
 
+
+    public void CategoryTextChanged()
+    {
+        LoadTextValue(dropdown.value); // Reload text when the category is changed
+    }
     public void LoadTextValue(int dropdownIndex)
     {
         string path = Path.Combine(UnityEngine.Application.persistentDataPath, "SaveData", title.text, "Chapters", dropdown.options[dropdownIndex].text);
@@ -33,7 +41,7 @@ public class LoadText : MonoBehaviour
         {
             string fileContents = File.ReadAllText(filePath);
             List<string> categoryTexts = SplitTextIntoCategories(fileContents);
-            text.text = categoryTexts[0];
+            text.text = categoryTexts[int.Parse(category.text)-1];
         }
         else
         {
@@ -49,31 +57,38 @@ public class LoadText : MonoBehaviour
     {
         List<string> strings = new List<string>();
         int i = 0;
-
         while (true)
         {
-            try
+            string startTag = $"[$%{{{i}}}%$]";
+            string endTag = $"]$%{{{i}}}%$[";
+
+            int startIndex = str.IndexOf(startTag);
+            int endIndex = str.IndexOf(endTag);
+
+            if (startIndex == -1 || endIndex == -1 || endIndex <= startIndex)
             {
-                string startTag = $"[$%{{{i}}}%$]";
-                string endTag = $"]$%{{{i}}}%$[";
-                int startIndex = str.IndexOf(startTag);
-                int endIndex = str.IndexOf(endTag);
-
-                if (startIndex == -1 || endIndex == -1)
-                {
-                    Debug.LogError(startTag);
-                    break;
-                }
-
-                strings.Add(str.Substring(startIndex + startTag.Length, endIndex - endTag.Length));
-
-                i++;
-            }
-            catch
-            {
-                Debug.LogError(str);
+                if (startIndex != -1) Debug.LogError($"Missing end tag {endTag} in string: " + str);
+                if (endIndex != -1) Debug.LogError($"Missing start tag {startTag} in string: " + str);
+                if (endIndex <= startIndex) Debug.LogError($"End tag {endTag} found before start tag {startTag} in string: " + str);
                 break;
             }
+
+            int substringStart = startIndex + startTag.Length;
+            int substringLength = endIndex - substringStart;
+
+            if (substringLength < 0)
+            {
+                Debug.LogError($"invalid tag found: start at index {startIndex} and end at {endIndex}. Full string: " + str);
+                break;
+            }
+
+            string extractedString = str.Substring(substringStart, substringLength);
+            strings.Add(extractedString);
+            Debug.Log($"Extracted string: {extractedString}");
+
+            str = str.Substring(endIndex + endTag.Length);
+
+            i++;
         }
 
         return strings;
