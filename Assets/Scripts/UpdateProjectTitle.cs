c1e8681dad72d3fae9d0ce6e76a4c73c5c9ccf33
Chriscraft57 <steiner.dichris@gmail.com>
2025-01-27 09:50:32 +0100

    Added lastEdit

--- Assets/Scripts/UpdateProjectTitle.cs
+++ Assets/Scripts/UpdateProjectTitle.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 public class UpdateProjectTitle : MonoBehaviour
 {
     public TMP_Text TMP_Text;
+    public TMP_Text LastEdit;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         var text = GetComponent<TMP_Text>();
         text.text = TMP_Text.text == "New Text" ? string.Empty : TMP_Text.text;
+        string folderPath = Application.persistentDataPath + "/SaveData/" + TMP_Text.text;
+        Debug.Log("FolderPath " + folderPath);
+        if (System.IO.Directory.Exists(folderPath))
+        {
+            var directoryInfo = new System.IO.DirectoryInfo(folderPath);
+            LastEdit.text = "Stand: " + directoryInfo.LastWriteTime.Date.ToString("dd.MM.yyyy");
+        }
+
+
+
     }
 }
